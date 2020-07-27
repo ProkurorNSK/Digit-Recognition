072c9e32fcf3041e157d921fa251209c66bb2e0e
pavel.fedyashin <proka@ngs.ru>
2020-07-27 16:00:09 +0700

    Neuron creator

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -1,8 +1,24 @@
 package recognition;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        int[] a = new int[9];
+        int[] w = {2, 1, 2, 4, -4, 4, 2, -1, 2};
+        int b = -5;
+        for (int i = 0; i < 9; i += 3) {
+            String text = scanner.next();
+            for (int j = 0; j < 3; j++) {
+                a[i + j] = text.charAt(j) == 'X' ? 1 : 0;
+            }
+        }
+
+        int result = b;
+        for (int i = 0; i < 9; i++) {
+            result += a[i] * w[i];
+        }
+        System.out.println(result > 0 ? 0 : 1);
     }
 }
